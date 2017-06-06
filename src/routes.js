1164f80897afa81ef755bae4c9ec1b6a0ba49c85
Cory Huckeba <coryhuckeba@Corys-MacBook-Pro.local>
2017-06-06 10:45:23 -0400

    Implemented proper routing using react-router and added 404 page

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var routes = (
+	<Route name="app" path="/" handler={require('./components/app')}>
+		<DefaultRoute handler={require('./components/homepage')} />
+		<Route name="authors" handler={require('./components/authors/authorPage')} />
+		<Route name="about" handler={require('./components/about/aboutpage')} />
+		<NotFoundRoute handler={require('./components/404')} />
+	</Route>
+);
+
+module.exports = routes;
